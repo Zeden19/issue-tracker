715a7a48a5a0ed2cbe47e47be95a10ddaeb914f2
arjunsharma <ashar562@uwo.ca>
2024-08-22 14:43:55 -0400

    Abstract error messages

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {Button, Callout, Text, TextField} from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { SubmitHandler, useForm, Controller } from "react-hook-form";
@@ -8,10 +8,11 @@ import { useRouter } from "next/navigation";
 import axios from "axios";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {createIssueSchema} from "@/app/validationSchemas";
+import { createIssueSchema } from "@/app/validationSchemas";
 import z from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
-type issueForm = z.infer<typeof createIssueSchema>
+type issueForm = z.infer<typeof createIssueSchema>;
 
 function NewIssue() {
   const [error, setError] = useState("");
@@ -21,7 +22,7 @@ function NewIssue() {
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm<issueForm>({resolver: zodResolver(createIssueSchema)});
+  } = useForm<issueForm>({ resolver: zodResolver(createIssueSchema) });
   const onSubmit: SubmitHandler<issueForm> = async (data) => {
     try {
       await axios.post("/api/issues", data);
@@ -41,14 +42,13 @@ function NewIssue() {
       <form onSubmit={handleSubmit(onSubmit)} className={"space-y-3"}>
         <h2>Create new Issue</h2>
         <TextField.Root {...register("title")} placeholder={"Issue Title"} />
-        {errors.title && <Text as={"p"} color={"red"}>{errors.title.message}</Text>}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           control={control}
           name={"description"}
           render={({ field }) => <SimpleMDE {...field} />}
         />
-        {errors.description && <Text as={"div"} color={"red"}>{errors.description.message}</Text>}
-
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button type={"submit"}>Submit New Issue</Button>
       </form>
     </div>
