956f3d6cc840204033fd68f5620f96da9e4ab329
arjunsharma <ashar562@uwo.ca>
2024-08-22 20:13:38 -0400

    Styling view issue page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,27 +1,39 @@
 import prisma from "@/prisma/prismaClient";
-import {notFound} from "next/navigation";
+import { notFound } from "next/navigation";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
-  params: {id: string}
+  params: { id: string };
 }
 
-async function IssueDetailPage({params} : Props) {
+async function IssueDetailPage({ params }: Props) {
   const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(params.id),
-    }
-  })
-  
+    },
+  });
+
   if (!issue) notFound();
   return (
     <div>
-      <p>{issue!.title}</p>
-      <p>{issue!.description}</p>
-      <p>{issue!.status}</p>
-      <p>{issue!.createdAt.toDateString()}</p>
-      <p>{issue!.updatedAt.toDateString()}</p>
+      <Heading
+        as={"h1"}
+        size={"8"}
+        className={"underline underline-offset-8 pb-2"}
+      >
+        {issue.title} &nbsp; &nbsp;
+      </Heading>
+      <Flex gap={"3"} my={"2"}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue!.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className={"prose dark:prose-invert"} mt={"4"}>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
-  )
+  );
 }
 
 export default IssueDetailPage;
