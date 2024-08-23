4fdf0d0b5b89355f211d997647465b67a3b76a1c
arjunsharma <ashar562@uwo.ca>
2024-08-23 18:55:30 -0400

    Add dialog confirmation

--- app/issues/[id]/DeleteButton.tsx
+++ app/issues/[id]/DeleteButton.tsx
@@ -1,11 +1,30 @@
-import {Button} from "@radix-ui/themes";
+import { Button, Dialog, Flex } from "@radix-ui/themes";
+import { Form } from "@radix-ui/react-form";
 
-function DeleteButton({issueId} : {issueId: number}) {
+function DeleteButton({ issueId }: { issueId: number }) {
   return (
-    <Button color={"red"}>
-        Delete
-    </Button>
-  )
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button color={"red"}>Delete</Button>
+      </Dialog.Trigger>
+
+      <Dialog.Content maxWidth={"250px"}>
+        <Dialog.Title>Are you Sure?</Dialog.Title>
+        <Dialog.Description size="2" mb="4">
+          This cannot be reverted
+        </Dialog.Description>
+
+        <Flex gap={"3"} justify={"end"}>
+          <Dialog.Close>
+            <Button color={"gray"}>Cancel</Button>
+          </Dialog.Close>
+          <Dialog.Close>
+            <Button color={"red"}>Delete</Button>
+          </Dialog.Close>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
 }
 
 export default DeleteButton;
